2fd84ce27513be47646cef74d9d7bd11bf3966d7
beckpaul <beckrpaul@gmail.com>
2024-03-11 13:44:53 -0400

    add baseline switch to change content based on found status of item

--- auction.js
+++ auction.js
@@ -17,11 +17,42 @@ const awaitLoadingCompleted = () => {
 
 const itemIsNew = (info) => {
   if (info.includes("item is new")) {
-    return true
+    return true;
   }
-  return false
-}
+  return false;
+};
 
+const itemStatuses = {
+  "item is new": true,
+  "This bulk item is sold as-is, and NO REFUNDS will be issued": true,
+  "Item Is In Retail Packaging. This Item Is Sold As-Is.": true,
+  "item is open box": true,
+};
+
+const getItemStatus = (info) => {
+  if (info.includes("item is new")) {
+    return 1;
+  } else if (info.includes("as-is")) {
+    return 2;
+  } else {
+    return 0;
+  }
+};
+
+const appendItemMarker = (element, background, textContent) => {
+  var newItemMarker = document.createElement("div");
+  newItemMarker.textContent = textContent;
+
+   var classes = ["btn", "pt-2", "text-center"];
+   classes.push(background);
+
+  newItemMarker.classList.add(...classes);
+  const elementToInsertBefore = element.querySelector(".tooltip-demos");
+  elementToInsertBefore.parentElement.insertBefore(
+    newItemMarker,
+    elementToInsertBefore
+  );
+};
 
 const setMarkers = () => {
   const auctionItems = document.getElementsByClassName(
@@ -33,11 +64,28 @@ const setMarkers = () => {
       .querySelector(".tooltip-demos")
       .innerHTML.toLowerCase();
 
-      if (itemIsNew(itemInformation)) {
-        console.info("New item detected");
-      }
+    const itemStatus = getItemStatus(itemInformation);
+    var background = "";
+
+    switch (itemStatus) {
+      case 1:
+        background = "bg-primary";
+        textContent = "New";
+        break;
+      case 2:
+        background = "bg-secondary";
+        textContent = "As-Is";
+        break;
+      default:
+        background = "bg-danger";
+        textContent = "Other";
+        break;
+    }
+
+    appendItemMarker(item, background, textContent);
   });
 };
+
 const setAuctionItemTooltips = () => {};
 document.addEventListener("DOMContentLoaded", [
   // console.info("extension loaded"),
